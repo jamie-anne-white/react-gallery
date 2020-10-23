4e0ec79952921e51804dc0e19a546d43f25edf1c
Jamie Anne White <jimmy.anne.white@gmail.com>
2020-10-23 10:02:55 -0500

    GalleryItem updated with styling

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,8 +1,7 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/folki_hat.jpg', description: 'A doggo in a hat', likes: 0 },
-    { id: 3, path: 'images/gibbs.jpg', description: 'A cat who wants to climb', likes: 0 },
-    { id: 4, path: 'images/theo_bath.jpg', description: 'A cat who hates a bath', likes: 0 }
+    { id: 1, path: 'images/folki_hat.jpg', description: 'A doggo in a hat', likes: 0 },
+    { id: 2, path: 'images/gibbs.jpg', description: 'A cat who wants to climb', likes: 0 },
+    { id: 3, path: 'images/theo_bath.jpg', description: 'A cat who hates a bath', likes: 0 }
 
 ];
 
--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './GalleryItem.css'
+
 
 
 class GalleryItem extends Component {
@@ -19,11 +21,13 @@ render() {
   console.log(this.props);
   return (
       <>
+      <div className='gallery-block'>
           {this.state.isPhoto ?
               <img src={this.props.picture.path} onClick={this.flipView} />
               : <p onClick={this.flipView}>{this.props.picture.description}</p>}
           <p>This photo has {this.props.picture.likes} likes.</p>
           <button onClick={() => this.props.addLike(this.props.picture)}>Like</button>
+          </div>
       </>
   );
 }
