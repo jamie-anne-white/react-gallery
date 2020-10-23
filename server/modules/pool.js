b15569f80791e5908de59cf5d09d5452a1b792e0
Jamie Anne White <jimmy.anne.white@gmail.com>
2020-10-23 16:02:25 -0500

    server is taking GET in gallery

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -2,12 +2,13 @@ const pg = require('pg');
 const Pool = pg.Pool;
 const config = {
     database: 'react_gallery', // the name of the database
-    host: 'Localhost', // where is your database
+    host: 'localhost', // where is your database
     port: 5432, // the port number for your database, 5432 is the default
     max: 10, // how many connections at one time
     idleTimeoutMillis: 30000 // 30 seconds to try to connect
 };
 
+// one instance to rule them all!
 const pool = new Pool(config);
 
 pool.on('connect', (client) => {
