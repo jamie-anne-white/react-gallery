e782021a4ce08d5189b6c51a3118c4244f766406
Jamie Anne White <jimmy.anne.white@gmail.com>
2020-10-23 15:49:07 -0500

    Working to update SQL for GET

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,12 +1,12 @@
-const galleryItems = [
-    { id: 1, path: 'images/folki_hat.jpg', description: 'A doggo in a hat', likes: 0 },
-    { id: 2, path: 'images/gibbs.jpg', description: 'A cat who wants to climb', likes: 0 },
-    { id: 3, path: 'images/theo_bath.jpg', description: 'A cat who does not like baths', likes: 0 },
-    { id: 4, path: 'images/hunger_horns_gr.jpeg', description: '7th street entry green room', likes: 0 },
-    { id: 5, path: 'images/hunger_horns_triplerock.jpeg', description: 'JJ&TH at Triple Rock', likes: 0 },
-    { id: 6, path: 'images/nick_jamie_hooters.jpeg', description: 'Hooters kiddos', likes: 0 },
-    { id: 7, path: 'images/utah_blm_land.jpeg', description: 'Van life!', likes: 0 }
+// const galleryItems = [
+//     { id: 1, path: 'images/folki_hat.jpg', description: 'A doggo in a hat', likes: 0 },
+//     { id: 2, path: 'images/gibbs.jpg', description: 'A cat who wants to climb', likes: 0 },
+//     { id: 3, path: 'images/theo_bath.jpg', description: 'A cat who does not like baths', likes: 0 },
+//     { id: 4, path: 'images/hunger_horns_gr.jpeg', description: '7th street entry green room', likes: 0 },
+//     { id: 5, path: 'images/hunger_horns_triplerock.jpeg', description: 'JJ&TH at Triple Rock', likes: 0 },
+//     { id: 6, path: 'images/nick_jamie_hooters.jpeg', description: 'Hooters kiddos', likes: 0 },
+//     { id: 7, path: 'images/utah_blm_land.jpeg', description: 'Van life!', likes: 0 }
 
-];
+// ];
 
-module.exports = galleryItems;
+// module.exports = galleryItems;
